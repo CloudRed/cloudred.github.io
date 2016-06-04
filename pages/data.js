058f1f29b07ff4dbc8fcbe57fe772112981c6b33
CloudRed <654892015@qq.com>
2016-06-04 15:34:03 +0800

    boring

--- pages/data.js
+++ pages/data.js
@@ -3,19 +3,19 @@ var page_data = [
     {
         title : '我的神奇的文章1',
         date : '2016-5-27',
-        path : 'pages/2016/demo1/',
+        path : '/pages/2016/demo1/',
         tags : ['DataBase', 'SQL Injection']
     },
     {
         title : '我的神奇的文章2',
         date : '2016-5-27',
-        path : 'pages/2016/demo2/',
+        path : '/pages/2016/demo2/',
         tags : ['SQL Injection']
     },
     {
         title : '我的神奇的文章3',
         date : '2016-5-28',
-        path : 'pages/2016/demo3/',
+        path : '/pages/2016/demo3/',
         tags : ['Math']
     },
     {
