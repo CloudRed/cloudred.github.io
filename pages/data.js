ee7fb5ca0c5cad82b3ea09d94974651bb90cff30
CloudRed <654892015@qq.com>
2016-06-04 18:13:09 +0800

    asdf

--- pages/data.js
+++ pages/data.js
@@ -3,13 +3,13 @@ var page_data = [
     {
         title : '我的神奇的文章1',
         date : '2016-5-27',
-        path : '/pages/2016/demo1/index.html',
+        path : '/pages/2016/demo1/',
         tags : ['DataBase', 'SQL Injection']
     },
     {
         title : '我的神奇的文章2',
         date : '2016-5-27',
-        path : '/pages/2016/demo2/index.html',
+        path : '/pages/2016/demo2/',
         tags : ['SQL Injection']
     },
     {
