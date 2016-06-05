35b4ffb306886786c0cf9db74ea8f0a01294f85e
CloudRed <654892015@qq.com>
2016-06-05 22:54:44 +0800

    update

--- pages/data.js
+++ pages/data.js
@@ -35,5 +35,65 @@ var page_data = [
         date : '2016-5-28',
         path : '/pages/2016/demo/',
         tags : ['Math']
+    },
+    {
+        title : '我的神奇的文章7',
+        date : '2016-5-28',
+        path : '/pages/2016/demo/',
+        tags : ['Math']
+    },
+    {
+        title : '我的神奇的文章8',
+        date : '2016-5-28',
+        path : '/pages/2016/demo/',
+        tags : ['Math']
+    },
+    {
+        title : '我的神奇的文章1',
+        date : '2016-5-27',
+        path : '/pages/2016/demo1/',
+        tags : ['DataBase', 'SQL Injection']
+    },
+    {
+        title : '我的神奇的文章2',
+        date : '2016-5-27',
+        path : '/pages/2016/demo2/',
+        tags : ['SQL Injection']
+    },
+    {
+        title : '我的神奇的文章3',
+        date : '2016-5-28',
+        path : '/pages/2016/demo3/',
+        tags : ['Math']
+    },
+    {
+        title : '我的神奇的文章4',
+        date : '2016-5-28',
+        path : '/pages/2016/demo4/',
+        tags : ['Math']
+    },
+    {
+        title : '我的神奇的文章5',
+        date : '2016-5-28',
+        path : '/pages/2016/demo5/',
+        tags : ['Math']
+    },
+    {
+        title : '我的神奇的文章6',
+        date : '2016-5-28',
+        path : '/pages/2016/demo/',
+        tags : ['Math']
+    },
+    {
+        title : '我的神奇的文章7',
+        date : '2016-5-28',
+        path : '/pages/2016/demo/',
+        tags : ['Math']
+    },
+    {
+        title : '我的神奇的文章8',
+        date : '2016-5-28',
+        path : '/pages/2016/demo/',
+        tags : ['Math']
     }
 ];
