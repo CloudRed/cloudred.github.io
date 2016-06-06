98ce7c3e3d8409aeb5d4bd27616d5292fc0b4da3
CloudRed <654892015@qq.com>
2016-06-06 23:07:08 +0800

    change

--- pages/data.js
+++ pages/data.js
@@ -7,10 +7,28 @@ var page_data = [
         tags : ['DataBase', 'SQL Injection']
     },
     {
+        title : '我的神奇的文章1',
+        date : '2016-5-27',
+        path : '/pages/2016/demo1/',
+        tags : ['DataBase', 'SQL Injection']
+    },
+    {
         title : '我的神奇的文章2',
         date : '2016-5-27',
         path : '/pages/2016/demo2/',
         tags : ['SQL Injection']
+    },
+    {
+        title : '我的神奇的文章2',
+        date : '2016-5-27',
+        path : '/pages/2016/demo2/',
+        tags : ['SQL Injection']
+    },
+    {
+        title : '我的神奇的文章3',
+        date : '2016-5-28',
+        path : '/pages/2016/demo3/',
+        tags : ['Math']
     },
     {
         title : '我的神奇的文章3',
@@ -46,24 +64,6 @@ var page_data = [
         title : '我的神奇的文章8',
         date : '2016-5-28',
         path : '/pages/2016/demo/',
-        tags : ['Math']
-    },
-    {
-        title : '我的神奇的文章1',
-        date : '2016-5-27',
-        path : '/pages/2016/demo1/',
-        tags : ['DataBase', 'SQL Injection']
-    },
-    {
-        title : '我的神奇的文章2',
-        date : '2016-5-27',
-        path : '/pages/2016/demo2/',
-        tags : ['SQL Injection']
-    },
-    {
-        title : '我的神奇的文章3',
-        date : '2016-5-28',
-        path : '/pages/2016/demo3/',
         tags : ['Math']
     },
     {
