70899216455df8f831d08ad385ba8bf602b59133
CloudRed <654892015@qq.com>
2016-06-08 19:44:35 +0800

    Normal Update
    - fixed bug
    - update the UI
    - fixed scrollbar, make it more sexy
    - otpimised js code, make it more reasonable

--- pages/data.js
+++ pages/data.js
@@ -1,11 +1,8 @@
-;
-var page_data = [
-    {
-        title : '我的神奇的文章1',
-        date : '2016-5-27',
-        path : '/pages/2016/demo1/',
-        tags : ['DataBase', 'SQL Injection']
-    },
+
+;var PAGE_SHOW = 8; // 每页显示的条目数
+
+
+;var PAGE_DATA = [
     {
         title : '我的神奇的文章1',
         date : '2016-5-27',
@@ -19,39 +16,9 @@ var page_data = [
         tags : ['SQL Injection']
     },
     {
-        title : '我的神奇的文章2',
-        date : '2016-5-27',
-        path : '/pages/2016/demo2/',
-        tags : ['SQL Injection']
-    },
-    {
-        title : '我的神奇的文章4',
+        title : '我的神奇的文章3',
         date : '2016-5-28',
-        path : '/pages/2016/demo4/',
-        tags : ['Math']
-    },
-    {
-        title : '我的神奇的文章5',
-        date : '2016-5-28',
-        path : '/pages/2016/demo5/',
-        tags : ['Math']
-    },
-    {
-        title : '我的神奇的文章6',
-        date : '2016-5-28',
-        path : '/pages/2016/demo/',
-        tags : ['Math']
-    },
-    {
-        title : '我的神奇的文章7',
-        date : '2016-5-28',
-        path : '/pages/2016/demo/',
-        tags : ['Math']
-    },
-    {
-        title : '我的神奇的文章8',
-        date : '2016-5-28',
-        path : '/pages/2016/demo/',
+        path : '/pages/2016/demo3/',
         tags : ['Math']
     },
     {
@@ -84,16 +51,4 @@ var page_data = [
         path : '/pages/2016/demo/',
         tags : ['Math']
     },
-    {
-        title : '我的神奇的文章3',
-        date : '2016-5-28',
-        path : '/pages/2016/demo3/',
-        tags : ['Math']
-    },
-    {
-        title : '我的神奇的文章3',
-        date : '2016-5-28',
-        path : '/pages/2016/demo3/',
-        tags : ['Math']
-    }
 ];
--- static/js/Generator.js
+++ static/js/Generator.js
@@ -1,26 +1,24 @@
 ;
 
-var PAGE_SHOW = 8; // 每页显示的条目数
+var page_show = PAGE_SHOW;
 var HOST_NAME = 'https://cloudred.github.io';
 
-;
-var data = page_data;
+var data = PAGE_DATA;
 var tags = [];
 var item_count = 0;
-var template = undefined;
-var comment_count = '//cloudred.disqus.com/count-data.js?';
+var template = g('#wrap').innerHTML;
+var DISQUSWIDGETS;
+var disqus_domain;
+var disqus_shortname = 'cloudred';
 
-
-
-template = g('#wrap').innerHTML;
-
+;
 var html = [];
-var show_length = PAGE_SHOW;
+var show_length = page_show;
 
-if( data.length < PAGE_SHOW ){
+if( data.length < page_show ){
     show_length = data.length;
 }
-if( data.length - item_count < PAGE_SHOW ){
+if( data.length - item_count < page_show ){
     show_length = data.length - item_count;
 }
 
@@ -39,78 +37,76 @@ item_count = i;
 g('#wrap').innerHTML = html.join(' ');
 
 /////////////////////////////////////////////////////////////
-    var DISQUSWIDGETS,disqus_domain;
-    disqus_shortname = 'cloudred';
 
-    var DISQUSWIDGETS = function(){
-        var f=document,
-            a=f.getElementById("dsq-count-scr"),
-            a=a&&a.getAttribute("src").match(/(https?:)?\/\/(?:www\.)?([\w_\-]+)\.((?:dev\.)?disqus\.(?:com|org)(?::\d+)?)/i),
-            e={},
-            s=f.head||f.body,
-            j={},
-            q={identifier:1,url:2};
 
-        e.domain=a&&a[3]||disqus_domain||"disqus.com";
-        e.forum=a&&a[2]||disqus_shortname;
-        e.proto=a&&a[1]||"";
-        e.getCount=function(b){
-            var c;
-            c=encodeURIComponent;
-            var a=e.proto+"//"+e.forum+"."+e.domain+"/count-data.js?",
-                d=[],
-                k=0,
-                l=10,
-                r="",
-                b=b||{};
-            b.reset && (j={},r="&_="+ +new Date);
-            for(var b=[f.getElementsByTagName("A"),f.getElementsByClassName&&f.getElementsByClassName("disqus-comment-count")||[]],m,i,g,h,n=0; n<b.length; n++){
-                m=b[n];
-                for(var o=0; o<m.length; o++){
-                    i=m[o];
-                    g=i.getAttribute("data-disqus-identifier");
-                    h=i.hash==="#disqus_thread"&&i.href.replace("#disqus_thread","")||i.getAttribute("data-disqus-url");
-                    if(g) h=q.identifier;
-                    else if(h) g=h,h=q.url;
-                    else continue;
+var DISQUSWIDGETS = function(){
+    var f=document,
+        a=f.getElementById("dsq-count-scr"),
+        a=a&&a.data.match(/(https?:)?\/\/(?:www\.)?([\w_\-]+)\.((?:dev\.)?disqus\.(?:com|org)(?::\d+)?)/i),
+        e={},
+        s=f.head||f.body,
+        j={},
+        q={identifier:1,url:2};
 
-                    var p;
-                    j.hasOwnProperty(g)?p=j[g]:(p=j[g]={elements:[],type:h},d.push(c(h)+"="+c(g)));
-                    p.elements.push(i)
-                }
+    e.domain=a&&a[3]||disqus_domain||"disqus.com";
+    e.forum=a&&a[2]||disqus_shortname;
+    e.proto=a&&a[1]||"";
+    e.getCount=function(b){
+        var c;
+        c=encodeURIComponent;
+        var a=e.proto+"//"+e.forum+"."+e.domain+"/count-data.js?",
+            d=[],
+            k=0,
+            l=10,
+            r="",
+            b=b||{};
+        b.reset && (j={},r="&_="+ +new Date);
+        for(var b=[f.getElementsByTagName("A"),f.getElementsByClassName&&f.getElementsByClassName("disqus-comment-count")||[]],m,i,g,h,n=0; n<b.length; n++){
+            m=b[n];
+            for(var o=0; o<m.length; o++){
+                i=m[o];
+                g=i.getAttribute("data-disqus-identifier");
+                h=i.hash==="#disqus_thread"&&i.href.replace("#disqus_thread","")||i.getAttribute("data-disqus-url");
+                if(g) h=q.identifier;
+                else if(h) g=h,h=q.url;
+                else continue;
+
+                var p;
+                j.hasOwnProperty(g)?p=j[g]:(p=j[g]={elements:[],type:h},d.push(c(h)+"="+c(g)));
+                p.elements.push(i)
             }
-            d.sort();
-            
-            for(c=d.slice(k,l); c.length; ) 
-                b=f.createElement("script"),b.src=a+c.join("&")+r,s.appendChild(b),k+=10,l+=10,c=d.slice(k,l)
-        };
-        e.displayCount=function(b){
-            for(var c,a,d,e=b.counts,b=b.text.comments; c=e.shift(); )
-                if(a=j[c.id]){
-                    switch(c.comments){
-                        case 0:d=b.zero;break;
-                        case 1:d=b.one;break;
-                        default:d=b.multiple
-                    }
-                    c=d.replace("{num}",c.comments);
-                    a=a.elements;
-
-                    for(d=a.length-1;d>=0;d--)
-                        a[d].innerHTML=c
+        }
+        d.sort();
+        
+        for(c=d.slice(k,l); c.length; ) 
+            b=f.createElement("script"),b.src=a+c.join("&")+r,s.appendChild(b),k+=10,l+=10,c=d.slice(k,l)
+    };
+    e.displayCount=function(b){
+        for(var c,a,d,e=b.counts,b=b.text.comments; c=e.shift(); )
+            if(a=j[c.id]){
+                switch(c.comments){
+                    case 0:d=b.zero;break;
+                    case 1:d=b.one;break;
+                    default:d=b.multiple
                 }
-        };
-        return e
-    }();
-    DISQUSWIDGETS.getCount();
+                c=d.replace("{num}",c.comments);
+                a=a.elements;
+
+                for(d=a.length-1;d>=0;d--)
+                    a[d].innerHTML=c
+            }
+    };
+    return e
+}();
+DISQUSWIDGETS.getCount();
 ////////////////////////////////////////////////////////////
 
 
 function addPage(){
     var html = [];
-    var show_length = PAGE_SHOW;
-    var host_name = HOST_NAME;
+    var show_length = page_show;
 
-    if( data.length - item_count < PAGE_SHOW ){
+    if( data.length - item_count < page_show ){
         show_length = data.length - item_count;
     }
 
